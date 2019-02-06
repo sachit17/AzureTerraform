6f5b7d6050523d1ae5b6c1fb74209b22f8308dc4
sachit17 <42463387+sachit17@users.noreply.github.com>
2019-02-06 17:31:59 +0530

    tf files added

---
+++ variables.tf
@@ -0,0 +1,28 @@
+#AzureRM
+variable "subscription_id" {
+  description = ""
+  default     = ""
+}
+
+variable "client_id" {
+  description = ""
+  default     = ""
+}
+
+variable "client_secret" {
+  description = ""
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = ""
+  default     = ""
+}
+
+variable "resource_group_name" {
+  description = "Resource Group"
+}
+
+variable "location" {
+  description = ""
+}
