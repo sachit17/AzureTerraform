b3f71c3f714a2af04a032b3c809d7c805643bd1e
sachit17 <42463387+sachit17@users.noreply.github.com>
2019-02-08 16:50:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "azurerm" {
-  subscription_id = "${subscription_id}"
-  client_id       = "${client_id}"
-  client_secret   = "${client_secret}"
-  tenant_id       = "${tenant_id}"
+  subscription_id = "$${subscription_id}"
+  client_id       = "$${client_id}"
+  client_secret   = "$${client_secret}"
+  tenant_id       = "$${tenant_id}"
 }
 
 resource "azurerm_resource_group" "demo_rg" {
-  name     = "${resource_group_name}"
-  location = "${location}"
+  name     = "$${resource_group_name}"
+  location = "$${location}"
 }
 
